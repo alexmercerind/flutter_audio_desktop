84afe3372ad9538b04db61f181ee5bd1d2a43f95
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2020-09-16 14:35:59 +0530

    👌 Added casting & fixed other warnings in VS. Using same source in both Linux & Windows

---
+++ audioplayer/audio.cpp
@@ -0,0 +1,55 @@
+/* http://github.com/alexmercerind/flutter_audio_desktop */
+
+#include "audioplayer.hpp"
+
+namespace Audio {
+    AudioPlayer* audioPlayer;
+
+    void initPlayer(int debug) {
+        audioPlayer = new AudioPlayer(static_cast<bool>(debug));
+    }
+
+    void loadPlayer(const char* fileLocation) {
+        audioPlayer->load(fileLocation);
+    }
+
+    void playPlayer() {
+        audioPlayer->play();
+    }
+
+    void pausePlayer() {
+        audioPlayer->pause();
+    }
+
+    void stopPlayer() {
+        audioPlayer->stop();
+    }
+
+    int getDuration() {
+        return audioPlayer->getDuration();
+    }
+
+    int getPosition() {
+        return audioPlayer->getPosition();
+    }
+
+    void setPosition(int timeMilliseconds) {
+        audioPlayer->setPosition(timeMilliseconds);
+    }
+
+    void setVolume(double volume) {
+        audioPlayer->setVolume(volume);
+    }
+
+    float getVolume() {
+        return audioPlayer->getVolume();
+    }
+}
+
+int main(int argc, const char** argv) {
+    AudioPlayer audioPlayer(true);
+    audioPlayer.load(argv[2]);
+    audioPlayer.play();
+    std::cin.get();
+    return 0;
+}
