01fd9155496a737dc45b1186c1f373dcee3d4ce5
alexmercerind <saini123hitesh@gmail.com>
2021-03-01 21:11:02 +0530

    👌 Added Flutter types for Windows implmentation

---
+++ windows/include/flutter_audio_desktop/flutter_types.hpp
@@ -0,0 +1,85 @@
+#include <string>
+#include <map>
+
+#include <windows.h>
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar_windows.h>
+#include <flutter/standard_method_codec.h>
+
+
+class Method {
+    public:
+    const flutter::MethodCall<flutter::EncodableValue>* methodCall;
+    std::string name;
+    std::map<flutter::EncodableValue, flutter::EncodableValue> arguments;
+    flutter::MethodResult<flutter::EncodableValue>* result;
+
+    Method(const flutter::MethodCall<flutter::EncodableValue>* methodCall, flutter::MethodResult<flutter::EncodableValue>* result) {
+        this->methodCall = methodCall;
+        this->name = std::string(this->methodCall->method_name());
+        this->arguments = std::get<flutter::EncodableMap>(*methodCall->arguments());
+        this->result = result;
+    }
+
+    template<class T>
+    void returnValue(T value);
+
+    template <>
+    void returnValue<int>(int value) {
+        this->result->Success(
+            flutter::EncodableValue(value)
+        );
+    }
+
+    template <>
+    void returnValue<std::string>(std::string value) {
+        const char* resultCString = value.c_str();
+        this->result->Success(
+            flutter::EncodableValue(resultCString)
+        );
+    }
+
+    template <>
+    void returnValue<std::map<std::string, std::string>>(std::map<std::string, std::string> value) {
+        auto map = flutter::EncodableMap();
+        for (const auto& pair : value) {
+            map[flutter::EncodableValue(pair.first)] = flutter::EncodableValue(pair.second);
+        }
+        this->result->Success(
+            flutter::EncodableValue(map)
+        );
+    }
+
+    void returnNull() {
+        this->result->Success(
+            flutter::EncodableValue(nullptr)
+        );
+    }
+
+    void returnNotImplemented() {
+        this->result->NotImplemented();
+    }
+
+    template<class T>
+    T getArgument(const char* argument);
+
+    template <>
+    int getArgument<int>(const char* argument) {
+        flutter::EncodableValue value = this->arguments[flutter::EncodableValue(argument)];
+        return std::get<int>(value);
+    }
+
+    template <>
+    std::string getArgument<std::string>(const char* argument) {
+        flutter::EncodableValue value = this->arguments[flutter::EncodableValue(argument)];
+        return std::get<std::string>(value);
+    }
+
+    template <>
+    float getArgument<float>(const char* argument) {
+        flutter::EncodableValue value = this->arguments[flutter::EncodableValue(argument)];
+        return static_cast<float>(std::get<double>(value));
+    }
+
+    void returnResult() {}
+};
